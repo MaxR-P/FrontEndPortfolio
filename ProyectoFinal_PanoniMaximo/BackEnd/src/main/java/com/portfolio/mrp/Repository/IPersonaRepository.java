8add615ea3ae452aac36dfefc4b21da65dcd4c06
MaxR-P <maximopagnoni@gmail.com>
2023-01-04 15:11:46 -0300

    Implementacion al FrontEnd y Routing

---
+++ ProyectoFinal_PanoniMaximo/BackEnd/src/main/java/com/portfolio/mrp/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+
+package com.portfolio.mrp.Repository;
+
+import com.portfolio.mrp.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+    
+}
