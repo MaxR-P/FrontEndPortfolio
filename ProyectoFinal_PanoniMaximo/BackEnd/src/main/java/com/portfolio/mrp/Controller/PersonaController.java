52245285b330004f67efae9b8a3cc7181d57edd2
MaxR-P <maximopagnoni@gmail.com>
2023-01-09 10:10:10 -0300

    Componentes educacion & experiencia agregados

--- ProyectoFinal_PanoniMaximo/BackEnd/src/main/java/com/portfolio/mrp/Controller/PersonaController.java
+++ ProyectoFinal_PanoniMaximo/BackEnd/src/main/java/com/portfolio/mrp/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfolio.mrp.Entity.Persona;
 import com.portfolio.mrp.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,17 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("/personas/traer")
+    @GetMapping("personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona) {
         ipersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/(id)")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
@@ -36,6 +39,7 @@ public class PersonaController {
     }
     
     //Url puerto/personas/editar/4/nombre & apelligo & img
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/(id)")
     public Persona editPersona(@PathVariable Long id, 
                                 @RequestParam("nombre") String nuevoNombre, 
